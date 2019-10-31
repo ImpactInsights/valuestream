be3b9c9f2116db40e485cc53fe2c4196b44e5b2e
dm03514 <dm03514@gmail.com>
2019-10-30 20:27:37 -0400

    Feature/jenkins generic webhook (#21)
    * Jenkins: New EventSource - Event & Source
    
    * Jenkins -> EventSource
    
    * Jenkins: Moved to EventSource

---
+++ eventsources/jenkins/source.go
@@ -0,0 +1,37 @@
+package jenkins
+
+import (
+	"encoding/json"
+	"github.com/ImpactInsights/valuestream/eventsources/webhooks"
+	"github.com/opentracing/opentracing-go"
+	"io/ioutil"
+	"net/http"
+)
+
+type Source struct {
+	tracer opentracing.Tracer
+}
+
+func (s Source) Name() string {
+	return "jenkins"
+}
+
+func (s *Source) Tracer() opentracing.Tracer {
+	return s.tracer
+}
+
+func (s *Source) Event(r *http.Request, payload []byte) (webhooks.Event, error) {
+	var be BuildEvent
+	err := json.Unmarshal(payload, &be)
+	return be, err
+}
+
+func (s *Source) ValidatePayload(r *http.Request, secretKey []byte) ([]byte, error) {
+	return ioutil.ReadAll(r.Body)
+}
+
+func NewSource(tracer opentracing.Tracer) (*Source, error) {
+	return &Source{
+		tracer: tracer,
+	}, nil
+}
